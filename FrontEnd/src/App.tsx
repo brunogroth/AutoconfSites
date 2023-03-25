14a9139021e35b9eb62fe997c343c2943a8f1e72
brunogroth <brunom.groth@gmail.com>
2023-03-25 20:56:07 -0300

    :seedling: First commit.

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,14 @@
+import { RouterProvider } from 'react-router-dom'
+import './App.css'
+import router from './router'
+
+function App() {
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
