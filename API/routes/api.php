4de8e90f896c63edc2126c48f06ca78b3faad3ae
brunogroth <brunom.groth@gmail.com>
2023-03-28 21:52:30 -0300

    :seedling: Laravel_Feat: User Controller&Requests, feat Resource for data transformation from api to client and API routes. React_Feat: receving users data from API, console.log everything :tada:

--- API/routes/api.php
+++ API/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')
     ->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
+        Route::apiResource('/users', UserController::class);
     });
 
 Route::get('/user', function (Request $request) {
@@ -27,3 +29,4 @@ Route::get('/user', function (Request $request) {
 Route::post('/signup', [AuthController::class, 'signup']);
 
 Route::post('/login', [AuthController::class, 'login']);
+
--- FrontEnd/src/views/Users.tsx
+++ FrontEnd/src/views/Users.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosClient from '../axios-client';
 
 export default function Users() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect( () => {
+    getUsers();
+  }, []);
+
+  const getUsers = () => {
+    setLoading(true);
+    axiosClient.get('/users')
+      .then(({data}) =>{
+        console.log(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }
   return (
     <div>
       Users Works
