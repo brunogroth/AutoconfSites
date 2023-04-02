bdc7b5b9bd20f42a5963ef6b89288e71ca919b4a
brunogroth <brunom.groth@gmail.com>
2023-04-01 22:34:43 -0300

    Fix: Routes and remove wrap from UserResource

--- API/routes/api.php
+++ API/routes/api.php
@@ -18,15 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')
     ->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::apiResource('/users', UserController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
     });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
+    Route::apiResource('/users', UserController::class);
 });
 
 Route::post('/signup', [AuthController::class, 'signup']);
-
 Route::post('/login', [AuthController::class, 'login']);
-
